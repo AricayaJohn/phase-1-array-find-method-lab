df804b7503a3a9105b2901ae36a0e5c07e7f0c8d
John Aricaya <jaricaya@gmail.com>
2024-02-18 12:11:59 -0800

    find array method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+const record = [
+    {year : "2015", result: "W"},
+    {year : "2014", result: "N/A"},
+    {year : "2013", result: "L"}
+]
+
+const superbowlWin = (yearWin) => {
+    const yearWon = yearWin.find(yearWin => yearWin.result === "W");
+    return yearWon ? yearWon.year : undefined;
+}
+
+console.log(superbowlWin(yearWin));
